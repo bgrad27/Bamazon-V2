8a777e635f7187cbe0ed4d8d85c079979483f7af
Brian Grad <briangrad@brians-mbp-2.attlocal.net>
2018-04-03 20:29:56 -0500

    Created connection

--- Bamazon-Supervisor/BamazonCustomer.js
+++ Bamazon-Supervisor/BamazonCustomer.js
@@ -1,1 +1,25 @@
-s
+// Need to require mysql to run database, inquirer for the prompt I'm going to run on the command line, and lastly console.table to show a table in my commandline
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+require("console.table");
+
+// Connect host with mysql
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "",
+    database: "bamazon"
+});
+
+// Connects server
+connection.connect(function (err) {
+    if (err) {
+        console.error("Error: " + err.stack);
+    }
+    loadProducts();
+});
